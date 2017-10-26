e6a39a35bb0eaa5a554515e24fbd86c5f30dfed9
foodvac <pianopat22@gmail.com>
2017-10-25 22:17:36 -0400

    DM mana regeneration over time and display text for mana count

---
+++ DungeonMaster/Assets/Scripts/DM/DmController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DmController : MonoBehaviour {
+
+	public Text manaText;
+
+	private int manaCount;
+
+	// Use this for initialization
+	void Start () {
+		manaCount = 0;
+		//every two seconds
+		InvokeRepeating ("IncremenetMana", 2.0f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//passive mana regen, updates UI element
+	void IncrementMana () {
+		manaCount += 1;
+		manaText.text = "Mana: " + manaCount.ToString();
+	}
+}

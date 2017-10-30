8c92eef72d9d1ddaaf6d13be7883d8791a48e036
foodvac <pianopat22@gmail.com>
2017-10-30 16:31:29 -0400

    MonsterSpawner class and DmController input

--- DungeonMaster/Assets/Scripts/DM/DmController.cs
+++ DungeonMaster/Assets/Scripts/DM/DmController.cs
@@ -5,25 +5,91 @@ using UnityEngine.UI;
 
 public class DmController : MonoBehaviour {
 
-	public Text manaText;
+	[SerializeField]
+	private Text manaText;
 
 	private int manaCount;
+	private int monsterToSummon;
+	private string zoneToSummon;
+	private bool toBeSummoned;
 
 	// Use this for initialization
 	void Start () {
 		manaCount = 0;
 		//every two seconds
-		InvokeRepeating ("IncremenetMana", 2.0f, 2.0f);
+		InvokeRepeating ("IncrementMana", 2.0f, 2.0f);
+		monsterToSummon = 0;
+		toBeSummoned = false;
+		zoneToSummon = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		// Check for monster summon by number
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			// Queue monster 1
+			toBeSummoned = true;
+			monsterToSummon = 1;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			// Queue monster 2
+			toBeSummoned = true;
+			monsterToSummon = 2;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			// Queue monster 3
+			toBeSummoned = true;
+			monsterToSummon = 3;
+		} else if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			// Queue monster 4
+			toBeSummoned = true;
+			monsterToSummon = 4;
+		}
+
+		// Check for summon zone
+		else if (Input.GetKeyDown ("q") && toBeSummoned) {
+			zoneToSummon = "q";
+			SummonMonster (monsterToSummon, zoneToSummon);
+		} else if (Input.GetKeyDown ("w") && toBeSummoned) {
+			zoneToSummon = "w";
+			SummonMonster (monsterToSummon, zoneToSummon);
+		} else if (Input.GetKeyDown ("e") && toBeSummoned) {
+			zoneToSummon = "e";
+			SummonMonster (monsterToSummon, zoneToSummon);
+		} else if (Input.GetKeyDown ("a") && toBeSummoned) {
+			zoneToSummon = "a";
+			SummonMonster (monsterToSummon, zoneToSummon);
+		} else if (Input.GetKeyDown ("s") && toBeSummoned) {
+			zoneToSummon = "s";
+			SummonMonster (monsterToSummon, zoneToSummon);
+		} else if (Input.GetKeyDown ("d") && toBeSummoned) {
+			zoneToSummon = "d";
+			SummonMonster (monsterToSummon, zoneToSummon);
+		}
+
+		// ESC key to cancel any queued actions
+		else if (Input.GetKeyDown (KeyCode.Escape)) {
+			monsterToSummon = 0;
+			toBeSummoned = false;
+			zoneToSummon = "";
+		}
 	}
 
-	//passive mana regen, updates UI element
+	// Passive mana regen, updates UI element
 	void IncrementMana () {
-		manaCount += 1;
+		ChangeMana (1);
+	}
+
+	void ChangeMana (int amount) {
+		manaCount += amount;
 		manaText.text = "Mana: " + manaCount.ToString();
+	}
+
+	void SummonMonster (int monster, string zone) {
+		int manaCost = monster;
+		
+		toBeSummoned = false;
+
+		// Deduct mana
+		ChangeMana(0 - manaCost);
 	}
 }
---
+++ DungeonMaster/Assets/Scripts/Monsters/MonsterSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private int xMin;
+
+	[SerializeField]
+	private int xMax;
+
+	[SerializeField]
+	private int yMin;
+
+	[SerializeField]
+	private int yMax;
+
+	Dictionary<string, Zone> zones;
+
+	// Use this for initialization
+	void Start () {
+		zones = new Dictionary<string, Zone>();
+
+		float xDiff = ((float)xMax - (float)xMin) / 3;
+		float yDiff = ((float)yMax - (float)yMin) / 2;
+
+		zones.Add("q", new Zone ((float)xMin, (float)xMin + xDiff, (float)yMax, (float)yMax - yDiff));
+		zones.Add("w", new Zone ((float)xMin + xDiff, (float)xMin + (2 * xDiff), (float)yMax, (float)yMax - yDiff));
+		zones.Add("e", new Zone ((float)xMin + (2 * xDiff), (float)xMax, (float)yMax, (float)yMax - yDiff));
+		zones.Add("a", new Zone ((float)xMin, (float)xMin + xDiff, (float)yMax - yDiff, (float)yMax - 2 * yDiff));
+		zones.Add("s", new Zone ((float)xMin, (float)xMin + xDiff, (float)yMax - yDiff, (float)yMax - 2 * yDiff));
+		zones.Add("d", new Zone ((float)xMin, (float)xMin + xDiff, (float)yMax - yDiff, (float)yMax - 2 * yDiff));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnMonster (int monster, string zone) {
+
+	}
+}
+
+public struct Zone {
+	public float xMin, xMax, yMin, yMax;
+
+	public Zone(float x1, float x2, float y1, float y2) {
+		xMin = x1;
+		xMax = x2;
+		yMin = y1;
+		yMax = y2;
+	}
+}

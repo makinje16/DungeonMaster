81ad9192389bfbc9bc5986ebf5a058ccdd887a5b
makinje <malcolm.akinje@gmail.com>
2017-11-06 12:02:39 -0500

    Current stage of the baseItem class and the placeHolder_item prefab. Have the collision detection working now just need to work on the effects.

---
+++ DungeonMaster/Assets/Scripts/Items/BaseItem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+
+public class BaseItem : MonoBehaviour
+{
+	private CircleCollider2D rangeCollider2D;
+	private HeroController _heroController;
+
+	[SerializeField] private float pickupRadius;
+
+	
+	public void ItemEffect()
+	{
+	}
+	
+	// Use this for initialization
+	void Start () {
+		_heroController = GameObject.Find("Hero").GetComponent<HeroController>();
+		rangeCollider2D = GetComponent<CircleCollider2D>();
+		rangeCollider2D.radius = pickupRadius;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (!other.gameObject.tag.Contains("Hero")) return;
+		gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

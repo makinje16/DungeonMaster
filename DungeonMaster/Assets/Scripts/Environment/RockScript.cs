9d444413c711b082a65b689e60379889a700e365
= <willowcarter42@gmail.com>
2017-11-14 14:27:23 -0500

    adding rock prefab

--- DungeonMaster/Assets/Scripts/Environment/RockScript.cs
+++ DungeonMaster/Assets/Scripts/Environment/RockScript.cs
@@ -6,7 +6,14 @@ public class RockScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+        if (Random.Range(0,10) > 5)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if(Random.Range(0, 10) > 5)
+        {
+            GetComponent<SpriteRenderer>().flipY = true;
+        }
     }
 	
 	// Update is called once per frame

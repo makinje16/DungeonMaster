79d7b59604958f0b052e214e9a85b08d0b57b52d
= <willowcarter42@gmail.com>
2017-11-14 14:50:08 -0500

    creating room spawner

---
+++ DungeonMaster/Assets/Scripts/Environment/RoomBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBuilder : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject rockbase;
+
+    [SerializeField]
+    private int width;
+    [SerializeField]
+    private int height;
+
+    [SerializeField]
+    private float startx;
+
+    [SerializeField]
+    private float starty;
+
+
+	// Use this for initialization
+	void Start () {
+        GameObject temprock;
+		for (int yn = 0; yn <= height; yn++)
+        {
+            
+            for (int xn = 0; xn <= width; xn++)
+            {
+                if (xn == 0 || xn == width || yn == 0 || yn == height)
+                {
+                    temprock = Instantiate(rockbase);
+                    temprock.transform.position = new Vector2(startx + xn, starty + yn);
+                }
+            }
+
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

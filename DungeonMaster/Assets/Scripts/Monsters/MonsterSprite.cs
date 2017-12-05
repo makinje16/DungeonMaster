c05fcb01974543a5f626db466f28b77598acf1e4
jeremysu1 <jeremycksu@yahoo.com>
2017-12-04 21:42:51 -0500

    Added A* and new scene

---
+++ DungeonMaster/Assets/Scripts/Monsters/MonsterSprite.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSprite : MonoBehaviour {
+    Quaternion rotation;
+	// Use this for initialization
+	void Start () {
+        rotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = rotation;
+	}
+}

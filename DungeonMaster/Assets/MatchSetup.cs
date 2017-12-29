f0014e3ed4030d3105e70347befe9ef60c18607b
makinje <malcolm.akinje@gmail.com>
2017-12-29 14:55:07 -0500

    Selection screen is sort of working. Now need to disable it after picking a player.

---
+++ DungeonMaster/Assets/MatchSetup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MatchSetup : NetworkBehaviour
+{
+
+	[SerializeField] private GameObject Hero;
+	
+	
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("SelectionScreen").GetComponent<SelectionScreen>().PlayerPrefab = this;
+	}
+
+	public void SpawnHero()
+	{
+		CmdSpawnHero(Hero);
+	}
+	
+	[Command]
+	public void CmdSpawnHero(GameObject hero)
+	{
+		GameObject heroGameObject = (GameObject)Instantiate(hero, transform.position, Quaternion.identity);
+		NetworkServer.SpawnWithClientAuthority(heroGameObject, this.gameObject);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
